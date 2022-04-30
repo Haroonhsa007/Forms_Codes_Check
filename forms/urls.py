ce8e5402cefb23cf206195a814c05abaaec2651f
David Martínez Rodríguez <damarro3@upv.es>
2022-04-30 23:43:50 +0200

    added home and about page to forms

--- forms/urls.py
+++ forms/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='forms-home'),
+    path('about/', views.about, name='forms-about'),
 ]
--- forms/views.py
+++ forms/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the forms index.")
+def home(request):
+    return HttpResponse("Hola mundo. Estás en la página de inicio")
+
+
+def about(request):
+    return HttpResponse("Hola, esta es la página sobre la información de los investigadores.")
