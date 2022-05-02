bdb158901142f2ec4f5feb4ab9189e503b325983
David Martínez Rodríguez <damarro3@upv.es>
2022-05-02 22:55:44 +0200

    created person model

--- forms/models.py
+++ forms/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+
+    men = 'M'
+    women = 'W'
+    sex_choices = [
+        (men, 'Men'),
+        (women, 'Women'),
+    ]
+    sex = models.CharField(
+        max_length=2,
+        choices=sex_choices,
+        null=True
+    )
+
+    age = models.PositiveSmallIntegerField(null=True)
