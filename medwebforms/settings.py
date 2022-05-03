b95cf3419441041f4377ce60d2baf47babb7dd0c
David Martínez Rodríguez <damarro3@upv.es>
2022-05-04 00:42:28 +0200

    added users app

--- medwebforms/settings.py
+++ medwebforms/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'harvest.apps.HarvestConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
