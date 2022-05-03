38136405f601726a14291422a10924181d1c2da1
David Martínez Rodríguez <damarro3@upv.es>
2022-05-03 10:52:25 +0200

    modified model

--- harvest/models.py
+++ harvest/models.py
@@ -5,14 +5,14 @@ class Person(models.Model):
 
     id = models.CharField(max_length=100, primary_key=True)
 
-    man = 'M'
-    woman = 'W'
+    man = 'Man'
+    woman = 'Woman'
     sex_choices = [
         (man, 'Man'),
         (woman, 'Woman'),
     ]
     sex = models.CharField(
-        max_length=2,
+        max_length=5,
         choices=sex_choices,
         null=True
     )
